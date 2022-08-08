3b157114bc2d3b0cfc8517e09b96c0ef067e668a
irfansami <irfasami00@gmail.com>
2022-08-09 01:04:24 +0700

    restore

---
+++ src/script/main.js
@@ -0,0 +1,51 @@
+$(document).ready(()=>{
+  $(window).scroll(()=>{
+    if(this.scrollY >20){
+      $('.navbar').addClass("sticky");
+    }else{
+      $('.navbar').removeClass("sticky");
+    }
+    if(this.scrollY > 500){
+      $('.scroll-up-btn').addClass("show");
+    }else{
+      $('.scroll-up-btn').removeClass("show");
+    }
+  });
+  //slide-up
+  $('.scroll-up-btn').click(()=>{
+    $('html').animate({scrollTop: 0});
+  });
+  //typed animation
+  let typed = new Typed(".typing",{
+    strings:["Web Developer","Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
+  // toggle navbar
+  $('.menu-btn').click(()=>{
+    $('.navbar .menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+  });
+  //owl carousel
+  $('.carousel').owlCarousel({
+    margin:20,
+    loop:true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive:{
+      0:{
+        items:1,
+        nav:false
+      },
+      600:{
+        items:2,
+        nav:false
+      },
+      1000:{
+        items:3,
+        nav:false
+      }
+    }
+  })
+})
